b106c0fa5e91d3046d80f49e7c8bcacd0a9e2afd
Michael Hofmair <hofmair@e-conomix.net>
2018-10-29 15:03:36 +0100

    :bug: Fix incorrect Value in $ZSH

--- zsh/init.sh
+++ zsh/init.sh
@@ -1,9 +1,14 @@
 echo "zsh.init"
+#.  .  .  .  .  .  .  .  .  .  .  .  .  .  .  .  .  .  .  .  .  .  .  .  .  .  .  .  .  .  .  .  .  .  .  .  .  .  .  .
+#
+##              ZSH.init
+#
+#.  .  .  .  .  .  .  .  .  .  .  .  .  .  .  .  .  .  .  .  .  .  .  .  .  .  .  .  .  .  .  .  .  .  .  .  .  .  .  .
 # If you come from bash you might have to change your $PATH.
 # export PATH=$HOME/bin:/usr/local/bin:$PATH
 
 # Path to your oh-my-zsh installation.
-  export ZSH="/home/inf8n8ty/.oh-my-zsh"
+  export ZSH="$HOME/.oh-my-zsh"
 
 # Set name of the theme to load --- if set to "random", it will
 # load a random theme each time oh-my-zsh is loaded, in which case,
