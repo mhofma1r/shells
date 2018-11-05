7f14ccb665ef422444ce8080358790b9b3595e51
Michael Hofmair <hofmair@e-conomix.at>
2018-11-05 13:13:40 +0100

    :sparkles: initialize zsh

--- zsh/init.sh
+++ zsh/init.sh
@@ -99,8 +99,9 @@ source $ZSH/oh-my-zsh.sh
 # plugins, and themes. Aliases can be placed here, though oh-my-zsh
 # users are encouraged to define aliases within the ZSH_CUSTOM folder.
 # For a full list of active aliases, run `alias`.
-#
+
 # Example aliases
+
 alias zshconfig="mate ~/.zshrc"
 alias ohmyzsh="mate ~/.oh-my-zsh"
 cd /var/www/projectPointer;
