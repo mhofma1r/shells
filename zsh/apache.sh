1fab69c4571dcb0f73285d5d3e694814c4e0479d
Michael Hofmair <hofmair@e-conomix.at>
2018-11-06 10:33:27 +0100

    :sparkles: Adds alias for zsh/apache

---
+++ zsh/apache.sh
@@ -0,0 +1,8 @@
+echo "init.zsh.apache"
+#.  .  .  .  .  .  .  .  .  .  .  .  .  .  .  .  .  .  .  .  .  .  .  .  .  .  .  .  .  .  .  .  .  .  .  .  .  .  .  .
+#
+##             APACHE
+#
+#.  .  .  .  .  .  .  .  .  .  .  .  .  .  .  .  .  .  .  .  .  .  .  .  .  .  .  .  .  .  .  .  .  .  .  .  .  .  .  .
+
+alias apch-conf="sudo nano /etc/apache2/sites-available/$1.conf"
