05ad3e6eebbf427ba596cf475ccbe024a77894cb
Michael Hofmair <hofmair@e-conomix.at>
2018-12-05 10:28:20 +0100

    new filsystem aliases

---
+++ zsh/fs-common.sh
@@ -0,0 +1,11 @@
+echo "init.zsh.fs-common"
+#.  .  .  .  .  .  .  .  .  .  .  .  .  .  .  .  .  .  .  .  .  .  .  .  .  .  .  .  .  .  .  .  .  .  .  .  .  .  .  .
+#
+##             FILESYSTEM
+#
+#.  .  .  .  .  .  .  .  .  .  .  .  .  .  .  .  .  .  .  .  .  .  .  .  .  .  .  .  .  .  .  .  .  .  .  .  .  .  .  .
+purge="rm -fdr "
+alias pwd="pwd -P"
+alias wsp="cd ~/workspace"
+alias prp="cd ~/projectPointer_$1"
+alias mkprp="ln -s ~/workspace/$1 ~/projectPointer_$1"
