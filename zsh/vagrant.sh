63611a73e970ca54bdebd2e9c07371af182223d6
Michael Hofmair <michael@hofmair.net>
2019-05-18 20:04:00 +0200

    Add's support for Macros

---
+++ zsh/vagrant.sh
@@ -0,0 +1,10 @@
+echo "init.zsh.vagrant"
+#.  .  .  .  .  .  .  .  .  .  .  .  .  .  .  .  .  .  .  .  .  .  .  .  .  .  .  .  .  .  .  .  .  .  .  .  .  .  .  .
+#
+##             APACHE
+#
+#.  .  .  .  .  .  .  .  .  .  .  .  .  .  .  .  .  .  .  .  .  .  .  .  .  .  .  .  .  .  .  .  .  .  .  .  .  .  .  .
+alias vg="vagrant"
+alias vgu="vagrant up"
+alias vgd="vagrant destroy"
+alias vgs="vagrant ssh"
